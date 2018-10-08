e0549b88478e453f4db97febb164e2756218bfcd
Zhenyu Zhang <shakuras@gmail.com>
2018-10-08 11:51:10 -0500

    change output metrics files

--- gistic_focal_threshold/threshold.gistic.focal.cnv.R
+++ gistic_focal_threshold/threshold.gistic.focal.cnv.R
@@ -1,8 +1,8 @@
 library(data.table)
 
 
-setwd("~/SCRATCH/gistic/data/pc_thresholded.by_genes")
-
+setwd("~/SCRATCH/gistic/data/pc_thresholded_03.by_genes")
+noise = 0.3
 
 ##########################################################
 # generate protein-coding gene list
@@ -44,8 +44,8 @@ for (i in 1:length(diseases)) {
   # separate data into metadata and copy number values, and generate thresholed values
   meta = focal[, 1:3]
   data = focal[, 4:ncol(focal)]
-  data = replace(data, data > 0, 1)
-  data = replace(data, data < 0, -1)
+  data = replace(data, data > noise, 1)
+  data = replace(data, data < -noise, -1)
 
   # collect metrics
   gene.gain.summary = gene.gain.summary + apply(data, 1, function(x) sum(x == 1))
@@ -64,8 +64,39 @@ for (i in 1:length(diseases)) {
   write.table(data, outfile, col.names=T, row.names=F, sep="\t", quote=F)
 }
 
+# write summary output
+# 11,368 aliquots and 19,729 protein coding genes in chr1-22/X
 
-UCS/focal_data_by_genes.txt
+gene.gain.summary = data.table(gene = names(gene.gain.summary), count = gene.gain.summary)
+write.table(gene.gain.summary, "gene.gain.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+gene.loss.summary = data.table(gene = names(gene.loss.summary), count = gene.loss.summary)
+write.table(gene.loss.summary, "gene.loss.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+gene.event.summary = gene.gain.summary
+gene.event.summary$count = gene.gain.summary$count + gene.loss.summary$count
+write.table(gene.event.summary, "gene.event.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+aliquot.gain.summary = data.table(aliquot = names(aliquot.gain.summary), count = aliquot.gain.summary)
+write.table(aliquot.gain.summary, "aliquot.gain.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+aliquot.loss.summary = data.table(aliquot = names(aliquot.loss.summary), count = aliquot.loss.summary)
+write.table(aliquot.loss.summary, "aliquot.loss.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+aliquot.event.summary = aliquot.gain.summary
+aliquot.event.summary$count = aliquot.gain.summary$count + aliquot.loss.summary$count
+write.table(aliquot.event.summary, "aliquot.event.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+write.table(project.summary, "project.summary.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+
+
+
+
+
+
+
+
 
 
 
